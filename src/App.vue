c2baf35002d6acd380c53c62a26766b4ff13ba8d
michaellarrubis <michaellarrubis@gmail.com>
2023-09-07 20:17:21 +0800

    Setup: Initial and Bare from Perch

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <VPost :post="post" />
+    <button @click="prevPost">Previous Post</button>
+    <button @click="nextPost">Next Post</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import VPost from './components/VPost.vue'
+import axios from 'axios'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    VPost
+  },
+  setup () {
+    const post = ref({});
+
+    (async () => {
+      // Here is the url for getting posts. The post id is at the end of the url.
+      const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts/1')
+      console.log(data)
+      post.value = data
+    })()
+
+    const prevPost = () => {
+      // TODO
+    }
+
+    const nextPost = () => {
+      // TODO
+    }
+
+    return {
+      post,
+      prevPost,
+      nextPost
+    }
+  }
+})
+</script>
---
+++ src/components/VPost.vue
@@ -0,0 +1,17 @@
+<template>
+  TODO: display the post
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'VPost',
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+})
+</script>
